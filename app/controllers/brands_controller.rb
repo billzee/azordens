7ee8ac6c9c8a5473ede553988d985e9ca3e8e02c
Guilherme Zordan <guilherme@inocode.com.br>
2016-09-21 15:59:10 -0300

    milestone

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -3,5 +3,16 @@ class BrandsController < ApplicationController
 		@brands = Brand.order :name
   	render "index"
 	end
+
+	def create
+    @client = Client.new(params[:client])
+    if @client.save
+      redirect_to @client
+    else
+      # This line overrides the default rendering behavior, which
+      # would have been to render the "create" view.
+      render "new"
+    end
+  end
 end
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,10 +6,6 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
-
-
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "bower_resources")
 
 # Precompile additional assets.
