a08389c6ba7b1177a28676858ce5d94d74a0678b
Guilherme Zordan <guilhermezordan@outlook.com>
2016-10-19 00:49:13 -0200

    linhas clicaveis

--- app/assets/javascripts/controllers/main.js
+++ app/assets/javascripts/controllers/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('azordens')
-  .controller('MainCtrl', function($scope, $window, swalService) {
+  .controller('MainCtrl', function($scope, $window, $location, swalService) {
     $('select').selectpicker();
 
-    $scope.delete = function(id, route){
-      swalService.delete(id, route);
-    }
-
+    $scope.go = function(route, id){
+      var path = route + '/' + id + '/edit';
+      $window.location.href = path;
+    };
   });
