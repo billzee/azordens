35808c852e4c1ece8524caaea526315789f747c4
Guilherme Zordan <guilhermezordan@outlook.com>
2016-10-24 20:25:17 -0200

    possbilidade de imprimir tables

--- app/assets/javascripts/controllers/main.js
+++ app/assets/javascripts/controllers/main.js
@@ -4,6 +4,10 @@ angular.module('azordens')
   .controller('MainCtrl', function($scope, $window, $location, swalService) {
     $('select').selectpicker();
 
+    $scope.print = function(){
+      $window.print();
+    }
+
     $scope.go = function(route, id){
       var path = route + '/' + id + '/edit';
       $window.location.href = path;
