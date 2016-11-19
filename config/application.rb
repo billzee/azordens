20ccc42fcc1722d0a6ef12f92f47b6959c68cfb7
Guilherme Zordan <guilhermezordan@outlook.com>
2016-11-19 10:40:50 -0200

    disable pre compile

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Azordens
     config.i18n.available_locales = ["pt-BR"]
     config.i18n.default_locale = :"pt-BR"
     config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
