d98b8859622072acc942c8093e954e59e3c03c24
Guilherme Zordan <guilhermezordan@outlook.com>
2016-11-25 00:32:27 -0200

    trabalho de testes

--- test/models/brand_test.rb
+++ test/models/brand_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class BrandTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "CT_003" do
+    brand = Brand.find_by(id: 1)
+    brand.update(visible: false)
+
+    assert_not brand.visible
+  end
 end
